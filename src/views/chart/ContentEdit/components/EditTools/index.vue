319edf6a9c0161939b0ba15315e1df9def6e94e8
QuietlyChan <1013893148@qq.com>
2024-04-09 16:00:17 +0800

    fix：修复编辑区域右下角工具栏样式不居中bug，并调整导入导出图标

--- src/views/chart/ContentEdit/components/EditTools/index.vue
+++ src/views/chart/ContentEdit/components/EditTools/index.vue
@@ -182,13 +182,13 @@ const btnList: BtnListType[] = [
     key: 'import',
     type: TypeEnum.IMPORTUPLOAD,
     name: '导入',
-    icon: ShareIcon
+    icon: DownloadIcon
   },
   {
     key: 'export',
     type: TypeEnum.BUTTON,
     name: '导出',
-    icon: DownloadIcon,
+    icon: ShareIcon,
     handle: exportHandle
   },
   {
@@ -231,6 +231,8 @@ $asideBottom: 70px;
   border: 1px solid;
   @include fetch-border-color('hover-border-color-shallow');
   &.aside {
+    display: flex;
+    justify-content: center;
     flex-direction: column-reverse;
     height: auto;
     right: 20px;
