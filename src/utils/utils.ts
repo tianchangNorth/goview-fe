0866ceccac3121ddf47d46249b0f12c9ae0d4799
奔跑的面条 <1262327911@qq.com>
2024-06-05 18:21:43 +0800

    feat: 标记分组预览时候的ID

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -25,7 +25,7 @@ export const isDev = () => {
  * @param { Number } randomLength
  */
 export const getUUID = (randomLength = 10) => {
-  return Number(Math.random().toString().substring(2, randomLength) + Date.now()).toString(36)
+  return 'id_' + Number(Math.random().toString().substring(2, randomLength) + Date.now()).toString(36)
 }
 
 /**
