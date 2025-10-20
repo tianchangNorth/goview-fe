911cb11bde0617f84404fb9fa5ebd7c5fa8c687e
xuchenyang <xuchenyang@openatom.org>
2025-10-20 16:12:09 +0800

    feat:将发布地址添加到路由白名单

--- src/router/router-guards.ts
+++ src/router/router-guards.ts
@@ -1,5 +1,5 @@
 import { Router } from 'vue-router';
-import { PageEnum, PreviewEnum } from '@/enums/pageEnum'
+import { PageEnum, PreviewEnum, PublishEnum } from '@/enums/pageEnum'
 import { loginCheck } from '@/utils'
 
 // 路由白名单
@@ -7,7 +7,9 @@ const routerAllowList = [
   // 登录
   PageEnum.BASE_LOGIN_NAME,
   // 预览
-  PreviewEnum.CHART_PREVIEW_NAME
+  PreviewEnum.CHART_PREVIEW_NAME,
+  // 发布
+  PublishEnum.CHART_PUBLISHED_NAME
 ]
 
 export function createRouterGuards(router: Router) {
